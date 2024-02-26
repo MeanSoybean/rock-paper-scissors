17e038761e434621e5533c2f6436dbef75dbaa87
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-26 12:00:14 +0700

    feat: Implement getComputerChoice to return a random rock/paper/scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,8 +2,17 @@ function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  }
+}
 
+let choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
+    let choice = choices[getRandomInt(0, choices.length)];
+    return choice;
+}
+
+let isContinuing = false;
+while (isContinuing) {
+    alert(getComputerChoice());
+    isContinuing = confirm("Continue getting choice?");
 
 }
